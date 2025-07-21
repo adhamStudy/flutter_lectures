cc51ad5ddd14a6deac96605b8ce1dda030dc68da
adham <adhm000111000111@gmail.com>
2025-07-22 01:12:22 +0300

    appbar done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,52 @@
-
+// Lecture 2 : Dart Basics
+import 'package:flutter/material.dart';
 void main() {
+
+  runApp(MyApp());
+
 }
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+      return MaterialApp(
+
+        debugShowCheckedModeBanner: false,
+       home: Scaffold(
+         appBar: AppBar(
+           backgroundColor: Colors.redAccent.shade200,
+           foregroundColor: Colors.white,
+           title:
+           Text('Lecture 3 ',
+             style: TextStyle(fontSize: 24,color: Colors.white,fontStyle:FontStyle.italic),
+           ),
+           // centerTitle: true,
+          actions: [
+            IconButton(onPressed: () {
+              print('Hi from button ');
+            }, icon: Icon(Icons.add))
+          ],
+         ),
+       ),
+
+      );
+  }
+}
+
+
+class MyText extends StatelessWidget {
+   MyText(this.text);
+   String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return    Padding(
+      padding: EdgeInsets.all(10),
+    child: Text(text,style: TextStyle(fontSize: 20),
+    ),
+    );
+  }
+}
+
 
