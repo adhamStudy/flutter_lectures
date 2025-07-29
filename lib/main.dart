4fda29e0402538608d174fb58a8288228740363b
adham <adhm000111000111@gmail.com>
2025-07-29 17:54:12 +0300

    lecture 1 start

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget{
         debugShowCheckedModeBanner: false,
        home: Scaffold(
          appBar: AppBar(
-           backgroundColor: Colors.redAccent.shade200,
+           backgroundColor: Colors.indigo,
            foregroundColor: Colors.white,
            title:
            Text('Lecture 3 ',
@@ -29,53 +29,17 @@ class MyApp extends StatelessWidget{
           ],
          ),
          body:
-      //    Text('Hello from body , Hello from body, Hello from body, Hello from body, Hello from body ',
-      // maxLines: 1,
-      //      style: TextStyle(fontSize: 20,color: Colors.red),
-      //      overflow: TextOverflow.fade,
-      //
-      //    ),
-      //    Container(
-      //      decoration: BoxDecoration(color: Colors.deepOrangeAccent,
-      //      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),topRight: Radius.circular(20)),
-      //
-      //
-      //
-      //      ),
-      //      margin: EdgeInsets.all(10),
-      //      width: 220,
-      //      height : 150,
-      //      // color: Colors.deepOrangeAccent,
-      //      child: Center(child: MyText('Body of the app'),),
-      //    )
-         Stack(children: [
-           Container(
-                decoration: BoxDecoration(color: Colors.grey.shade300,
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),topRight: Radius.circular(20)),
-                ),
-                margin: EdgeInsets.all(10),
-                width: 220,
-                height : 150,
-                // color: Colors.deepOrangeAccent,
-                child: Center(child: MyText('Body of the app'),),
-              ),
 
-
-           Positioned(
-             top: 110,
-             left: 180,
-             child: Container(
-               width:60,
-               height: 60,
-               child: Center(child: Text('30%',style: TextStyle(color: Colors.white,fontSize: 20),),),
-               decoration: BoxDecoration(color: Colors.green,
-                   border: Border.all(width: 5,color: Colors.white),
-
-                   borderRadius: BorderRadius.circular(50)
-               ),
-             ),
+         Container(
+           decoration: BoxDecoration(color: Colors.grey.shade300,
+             borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),topRight: Radius.circular(20)),
            ),
-         ],)
+           margin: EdgeInsets.all(10),
+           width: 220,
+           height : 150,
+           // color: Colors.deepOrangeAccent,
+           child: Center(child: MyText('Body of the app'),),
+         ),
       ),
       )
     ;
