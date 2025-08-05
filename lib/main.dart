b45c3b79e10f990fe5cd72b86d2c4efcf64e9407
adham <adhm000111000111@gmail.com>
2025-08-06 01:10:17 +0300

    lecture5 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,58 @@
-// Lecture 2 : Dart Basics
+// Lecture 5 : List View + ListView Builder
 import 'package:flutter/material.dart';
+import 'package:flutter_lectures/pages/home_page.dart';
+import 'package:flutter_lectures/pages/second_page.dart';
 void main() {
   print('test');
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
-
+List <String> images = ['0.png','1.png','2.png','3.png','4.png'];
   @override
   Widget build(BuildContext context){
       return MaterialApp(
 
         debugShowCheckedModeBanner: false,
-       home: Scaffold(
-
-         appBar: AppBar(
-           backgroundColor: Colors.indigo,
-           foregroundColor: Colors.white,
-           title:
-           Text('Lecture 3 ',
-             style: TextStyle(fontSize: 24,color: Colors.white,fontStyle:FontStyle.italic),
-           ),
-           // centerTitle: true,
-          actions: [
-            IconButton(onPressed: () {
-              print('Hi from button ');
-            }, icon: Icon(Icons.add))
-          ],
-         ),
-         body:
-           Container(
-             height: double.infinity,
-             color: Colors.blueGrey.shade100,
-child:
-               // Column(
-               //   mainAxisAlignment: MainAxisAlignment.center,
-               //   crossAxisAlignment: CrossAxisAlignment.start,
-               //   children: [
-               //     MyText('First'),
-               //     MyText('First'),
-               //     MyText('First'),
-               //   ],
-               // )
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          MyText('First'),
-          MyText('Second'),
-          MyText('Third')
-        ],
-      )
-             // ListView(
-             //   scrollDirection: Axis.vertical,
-             //
-             //
-             //   children: [
-             //    ListTile(title: Text('first'),
-             //    subtitle: Text('subtitle'),
-             //    leading: Icon(Icons.favorite),
-             //      trailing: Icon(Icons.delete,color: Colors.red,),
-             //    ),
-             //     ListTile(title: Text('first'),
-             //       subtitle: Text('subtitle'),
-             //       leading: IconButton(onPressed: (){
-             //         print('hello from leading');
-             //       }, icon: Icon(Icons.add)),
-             //       trailing: Icon(Icons.delete,color: Colors.red,),
-             //     ),
-             //
-             //
-             //
-             //
-             //
-             //   ],
-             // ),
-           )
-
-
-         // Container(
-         //   width: 250,
-         //   child:
-         //   Stack(
-         //     children: [
-         //       Container(
-         //         decoration: BoxDecoration(
-         //          gradient: LinearGradient(colors: [Colors.white,Colors.grey.shade300]),
-         //           borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30),topRight: Radius.circular(30)),
-         //         ),
-         //         margin: EdgeInsets.all(10),
-         //         width: 220,
-         //         height : 150,
-         //         // color: Colors.deepOrangeAccent,
-         //         child: Center(child: Text('Hello From container',style: TextStyle(fontSize: 20,color: Colors.black),),),
-         //       ),
-         //       Positioned(
-         //           top: 10,
-         //           left: 200,
-         //           child: Container(
-         //
-         //             decoration: BoxDecoration(color: Colors.indigo,
-         //                 borderRadius: BorderRadius.circular(25),
-         //               border: Border.all(color: Colors.white,width: 4)
-         //
-         //             ),
-         //             width: 50,height: 50,
-         //           child: Center(child: Text('30%',style: TextStyle(color: Colors.white,fontSize: 16,
-         //           ),
-         //           ),
-         //           )
-         //           ),
-         //
-         //       )
-         //     ],
-         //   ),
-         // )
-      ),
+       home: Home()
       )
     ;
   }
 }
 
 
-class MyText extends StatelessWidget {
-   MyText(this.text);
-   String text;
+class Persons extends StatelessWidget {
+  const Persons({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return    Padding(
-      padding: EdgeInsets.all(4),
-    child: Text(text,style: TextStyle(fontSize: 20),
-    ),
+    return Container(
+      height: 120,
+      color:Colors.indigo.shade100,
+      child:
+      ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.all(10),
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(color: Colors.green,width: 3),
+                color: Colors.white,
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/${index}.png'))
+            ),
+          )
+          ;
+        },
+        scrollDirection: Axis.horizontal,
+
+      )
+      ,
     );
   }
 }
-
-
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lectures/pages/second_page.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+          title:
+          Text('Lecture 5 ',
+            style: TextStyle(fontSize: 24,color: Colors.white,fontStyle:FontStyle.italic),
+          ),
+          // centerTitle: true,
+          actions: [
+            IconButton(onPressed: () {
+              print('Hi from button ');
+            }, icon: Icon(Icons.add))
+          ],
+        ),
+        body:
+        ListView(
+
+          children: [
+            Container(
+              height: 120,
+              color:Colors.indigo.shade100,
+              child:
+              ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(color: Colors.green,width: 3),
+                        color: Colors.white,
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/${index}.png'))
+                    ),
+                  )
+                  ;
+                },
+                scrollDirection: Axis.horizontal,
+
+              )
+              ,
+            ),
+            SizedBox(height: 20,),
+            Align(
+              alignment: Alignment.center,
+              child:
+              Container(
+                width: 200,
+                child: ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage(),));
+                }, child: Text('click me')),
+              ),
+            )
+
+
+          ],
+        )
+
+    );
+  }
+}
